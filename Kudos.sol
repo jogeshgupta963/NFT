3537240b2b261ba2bb11af2955b076174b55024a
Jogesh Gupta <88982643+jogeshgupta963@users.noreply.github.com>
2022-05-07 13:00:53 +0530

    Update Kudos.sol

--- Kudos.sol
+++ Kudos.sol
@@ -1,20 +1,42 @@
- pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >=0.7.0 <0.9.0;
 
-  contract  Kudos {
-      mapping (address => Kudo[]) allKudos;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+  contract  Kudos is ERC721{
+
+      constructor() ERC721("kudos","mjk"){
+
+      }
+
+
+      mapping (address => uint[]) allKudos;
+      mapping(uint => Kudo) nfts;
+      uint public nextTokenId = 0;
 
       function giveKudos(address who,string memory what,string memory comment) public {
-          Kudo memory kudo = Kudo(what,msg.sender,comment);
-          allKudos[who].push(kudo);
+          Kudo memory kudo = Kudo(what,msg.sender,comment,nextTokenId);
+          
+          _mint(who,nextTokenId);
+          nfts[nextTokenId] = kudo;
+
+            allKudos[who].push(nextTokenId);
+
+          nextTokenId = nextTokenId+1;
+          
       }
 
     function getKudosLength(address who) public view returns(uint){
-        Kudo[] memory kudosForWho = allKudos[who];
+        uint[] memory kudosForWho = allKudos[who];
         return kudosForWho.length;
     }
 
       function getKudosAtIndex(address who,uint idx) public view returns(string memory,address,string memory){
-          Kudo memory kudo = allKudos[who][idx];
+          Kudo memory kudo = nfts[allKudos[who][idx]];
+          return (kudo.what,kudo.giver,kudo.comment);
+      }
+
+      function getNftInfo(uint _tokenId) public view returns(string memory,address,string memory){ 
+           Kudo memory kudo = nfts[_tokenId];
           return (kudo.what,kudo.giver,kudo.comment);
       }
   }
@@ -23,7 +45,5 @@
       string what;
       address giver;
       string comment;
+      uint nftTokenId;
   }
-
-  // giving to ->0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
-  // from ->0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
